c8c658e1a0901118dcd4183b74d4ace043fd18d7
celebrated-strategy-2405 <robertaledo22@gmail.com>
2020-03-09 06:37:36 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,6 +2,14 @@
 #phrase "Wingardium Leviosa"
 #increment your levation force by 1
 
+def using_until
+  levitation_force = 6
+  until levitation_force >= 10
+   phrase "Wingardium Leviosa"
+   levitation_force += 1
+ end
+end
+  
 
 
     
